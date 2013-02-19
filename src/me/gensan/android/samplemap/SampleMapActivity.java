0c29ba6819837a53e273a2b8699831f3b9c31c91
nasneg <hashizume.masaru@gmail.com>
2013-02-20 03:03:33 +0900

    Google Maps Android API v2 : API 12 or later.

---
+++ src/me/gensan/android/samplemap/SampleMapActivity.java
@@ -0,0 +1,15 @@
+
+package me.gensan.android.samplemap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleMapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_map);
+    }
+
+}
